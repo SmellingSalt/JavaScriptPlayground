7416cdb8b26b77df6d20b1ad4be007f9c71885ea
Sawan <ssmahara96@gmail.com>
2021-06-30 12:05:26 +0000

    Simple Server

--- FirstCode.js
+++ FirstCode.js
@@ -1,2 +1,10 @@
-let k = true
-console.log(true)
+var http=require("http")
+
+http.createServer(
+    function(req,res)
+{
+    res.writeHead(200,{"Content-type":"HTML"})
+    res.write("Hello")
+    res.end()
+}   
+).listen(8080)
