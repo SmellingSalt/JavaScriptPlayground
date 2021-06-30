1b068d97b1f366faf9ed140be33106ef01e378ce
Sawan <ssmahara96@gmail.com>
2021-06-30 12:25:34 +0000

    comments

--- FirstCode.js
+++ FirstCode.js
@@ -1,10 +1,11 @@
-var http=require("http")
-
+var http=require("http") //Importing the http module
+/*http.createServer is a function that creates the server.
+It needs a function as the argument*/
 http.createServer(
-    function(req,res)
+    function(req,res)//This function gets a request and responds with res
 {
-    res.writeHead(200,{"Content-type":"HTML"})
-    res.write("Hello")
-    res.end()
+    res.writeHead(200,{"Content-type":"HTML"}) //Header for the data. 200 is the header code
+    res.write("Hello") //Message
+    res.end() // End the session for the client it has sent what it needs to send.
 }   
-).listen(8080)
+).listen(8080) //listen on port 8080.
